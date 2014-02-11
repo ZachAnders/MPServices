099262ca3d9e749e37d3ef90931066cf330a503a
ZachAnders <Zacharia.Anders@gmail.com>
2014-02-11 11:48:26 -0700

    Refactored to accept arguments. Added Usage.

--- mpservices.py
+++ mpservices.py
@@ -15,23 +15,31 @@ def get_running_daemons():
 	daemons = [line.split("\t") for line in daemons]
 	daemons = [line[2].strip() for line in daemons[1:]]
 	return daemons
+
+def usage(outpipe):
+	sname = str(sys.argv[0])
+	outpipe.write("Lists or starts/stops macports related services.\n")
+	outpipe.write("Usage: ./" + sname + " [<service name> <up/down>] \n")
 	
-
-def main(outpipe):
-	ls = os.listdir(DAEMONS_DIR)
-
-	macports_daemons = [daemon.replace(".plist", "") for daemon in ls if daemon.find("org.macports") == 0]
+def print_services(outpipe, macports_daemons):
 	running_daemons = [mpdaemon for mpdaemon in get_running_daemons() if "org.macports" in mpdaemon]
 	for daemon in macports_daemons:
-		outs = daemon + " "*(60-len(daemon))
-		outpipe.write(outs)
 		if daemon in running_daemons:
 			col = COL_GRN
 			status = "RUNNING"
 		else:
 			col = COL_RED
 			status = "NOT RUNNING"
-		outpipe.write("".join([col, status, COL_END, "\n"]))
+		outpipe.write(col + status + COL_END + "\n")
+
+def main(outpipe):
+	ls = os.listdir(DAEMONS_DIR)
+
+	macports_daemons = [daemon.replace(".plist", "") for daemon in ls if "org.macports." in daemon]
+	if len(sys.argv) == 1:
+		print_services(outpipe, macports_daemons)
+	else:
+		usage(outpipe)
 
 if __name__ == "__main__":
 	main(sys.stdout)
